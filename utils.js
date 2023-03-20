8b816e750f52d150951a54a8c49e5b576d688631
renmu123 <1101022351@qq.com>
2023-03-20 13:32:57 +0800

    修改实现方式

--- utils.js
+++ utils.js
@@ -1,11 +1,7 @@
 const fs = require("fs");
 const path = require("path");
-const assParser = require("ass-parser");
-const assStringify = require("ass-stringify");
 
-const dayjs = require("dayjs");
-const customParseFormat = require("dayjs/plugin/customParseFormat");
-dayjs.extend(customParseFormat);
+const { parse, stringify, compile, decompile } = require("ass-compiler");
 
 const moveRegex = /{\\move\((.*?)\)\\(.+?)}/g;
 
@@ -31,39 +27,31 @@ const convertAss = (
   const assContent = fs.readFileSync(input, "utf8");
 
   // 解析Ass文件
-  const assData = assParser(assContent);
+  const assData = parse(assContent);
   const items = [];
-  for (const data of assData) {
-    if (data.section === "Events") {
-      for (const [index, item] of Object.entries(data.body)) {
-        const value = item.value;
-        if (value.Style === "message_box" && value.Text.includes("clip")) {
-          continue;
-        }
-        if (value.Style === "message_box" && value.Text.includes("move")) {
-          const startTime = dayjs(value.Start, "H:mm:ss.SS");
-          const endTime = dayjs(value.End, "H:mm:ss.SS");
-          // value.Start = startTime.add(options.duration, "second").format("H:mm:ss.SSS");
-          value.End = endTime
-            .add(options.duration, "second")
-            .format("H:mm:ss.SSS");
-          value.Text = value.Text.replace(moveRegex, (match, p1, p2) => {
-            const [x1, y1, x2, y2] = p1.split(",");
-            return `{\\pos(${x2},${y2})\\${p2}}`;
-          });
-        }
-        items.push(item);
-      }
-      data.body = items;
+  // console.log(assData);
+  for (const item of assData["events"]["dialogue"]) {
+    // console.log(item);
+    if (item.Style === "message_box" && item.Text.raw.includes("clip")) {
+      continue;
     }
+    if (item.Style === "message_box" && item.Text.raw.includes("move")) {
+      item.End = item.Start + options.duration;
+      item.Text.raw = item.Text.raw.replace(moveRegex, (match, p1, p2) => {
+        const [x1, y1, x2, y2] = p1.split(",");
+        return `{\\pos(${x2},${y2})\\${p2}}`;
+      });
+    }
+    items.push(item);
   }
+  assData["events"]["dialogue"] = items;
 
   // 将修改后的Ass数据转换回字符串
-  const newAssContent = assStringify(assData);
+  const newAssContent = stringify(assData);
 
   // 将修改后的Ass数据写入到新文件中
   fs.writeFileSync(outputPath, newAssContent, "utf8");
 };
 
-// convertAss("example.ass");
+convertAss("example.ass");
 module.exports = convertAss;
